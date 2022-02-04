6aaef187cc613f1bcc7451a735340f47cd453793
Paweł Ciejka <pawel.ciejka@robco.pl>
2022-02-04 23:27:48 +0000

    object oriented architecture ready for testing

--- SensorWatch.py
+++ SensorWatch.py
@@ -46,22 +46,17 @@ class ThreadedStreamRequestHandler(socketserver.BaseRequestHandler):
 class ThreadedStreamServer(socketserver.ThreadingMixIn, socketserver.UnixStreamServer):
     pass
 
-
-
-
 class Application:
     def __init__(self):
-        ### Global declarations
-        time_queue = Queue()
-        machine = MotionSensor(args.pin) if not args.test else 0
-        consumer_lock = Lock()
-        consumer_lock.acquire()
-        th = Thread(target = sensor_motion_consumer, args = ( args.output, args.resolution, args.machname))
+        self.args = self.parse_arguments()
+        self.check_env()
+        self.signal_setup()
+        self.sensor = PiMotionSensor(self.args)
 
     """
     Parse command line arguments
     """
-    def parse_argumnets(self):
+    def parse_arguments(self):
         print("Parsing command line arguments")
         parser = argparse.ArgumentParser(description='Program that watches changes on Motion Sensor on a given RBPI PIN')
         parser.add_argument('--pin', default = 18, help = "number of PIN to which sensor is connected")
@@ -73,33 +68,27 @@ class Application:
         return parser.parse_args()
 
     def check_env(self):
-        print("")
-        if not args.test:
-            #RBPI dev related imports only if not in test mode
-            from gpiozero import MotionSensor
-
+        print("Checking environment")
         #check and create if necessary instance directory
-        if not os.path.isdir(args.output):
-            print("CSV storage path:",args.output,"does not exists or is not a directory")
-            print("Please create it using `sudo mkdir",args.output,"`")
-            sys.exit()
+        if not os.path.isdir(self.args.output):
+            print("CSV storage path:",self.args.output,"does not exists or is not a directory")
+            print("Please create it using `sudo mkdir",self.args.output,"`")
             return False
 
-        if not os.access(args.output, os.W_OK):
-            print("CSV storage directory:",args.output,"is not writeable")
-            print("Please change ownership of this directory using: `sudo chown {}:{} {}`".format(getpass.getuser(),getpass.getuser(),args.output))
-            print("also change permission of this directory using: `sudo chmod 750 {}`".format(args.output))
-            sys.exit()
+        if not os.access(self.args.output, os.W_OK):
+            print("CSV storage directory:",self.args.output,"is not writeable")
+            print("Please change ownership of this directory using: `sudo chown {}:{} {}`".format(getpass.getuser(),getpass.getuser(),self.args.output))
+            print("also change permission of this directory using: `sudo chmod 750 {}`".format(self.args.output))
             return False
         
-        if not os.path.isdir(os.path.join(args.output, "machine_{}".format(args.machname))):
-            os.mkdir(os.path.join(args.output, "machine_{}".format(args.machname)))
+        if not os.path.isdir(os.path.join(self.args.output, "machine_{}".format(self.args.machname))):
+            os.mkdir(os.path.join(self.args.output, "machine_{}".format(self.args.machname)))
+        return True
     
     #Catch main thread interrupt to perform graceful exit
     def signal_handler(self,signum, frame):
         print("Shutdown signal received")
-        consumer_lock.release()
-        th.join()
+        self.sensor.stop()
         print("exiting")
         sys.exit()
     ###
@@ -107,22 +96,37 @@ class Application:
     def signal_setup(self):
         print("Performing signal handler setup")
         ### Main thread OS signal handle
-        signal.signal(signal.SIGHUP,signal_handler)
-        signal.signal(signal.SIGINT,signal_handler)
-        signal.signal(signal.SIGTERM,signal_handler)
+        signal.signal(signal.SIGHUP,self.signal_handler)
+        signal.signal(signal.SIGINT,self.signal_handler)
+        signal.signal(signal.SIGTERM,self.signal_handler)
         
-    def sensor_motion_exec():
-        th.start()
+    def run(self):
+        print("Application Run")
         pause()
         #this should never happen
         #th.join()
 
 
-class MotionSensor:
-    def __init__(self):
+class PiMotionSensor:
+    def __init__(self,args):
+        self.time_queue = Queue()
+        self.consumer_lock = Lock()
+        self.consumer_lock.acquire()
+        self.args = args
+        self.data = numpy.zeros(24)
         
+        if not args.test:
+            #RBPI dev related imports only if not in test mode
+            from gpiozero import MotionSensor
+            self.sensor = MotionSensor(args.pin)
+            self.th = Thread(target = self.consumer, args = ( args.output, args.resolution, args.machname))
 
-    def tofile(self,output,machname,data):
+    def stop(self):
+        self.consumer_lock.release()
+        if not self.args.test:
+            self.th.join()
+
+    def to_file(self,output,machname,data):
         print("Writing uptime statistics to file {}".format(output))
         total = 0
         with open(output,mode="w",encoding="utf-8") as fd:
@@ -134,7 +138,7 @@ class MotionSensor:
             fd.write("Total, {:.2f}\r\n".format(total))
         fd.close()
 
-    def tostdout(self,machname,data):
+    def to_stdout(self,machname,data):
         print("Writing uptime statistics to stdout")
         total = 0
         print("Machine name, {}".format(machname))
@@ -149,9 +153,8 @@ class MotionSensor:
     """
     def consumer(self,output, resolution, machname):
         print("consumer thread started")
-        data = numpy.zeros(24)
+        
         now = datetime.datetime.now()
-            
         laston = 0
         hidx = now.hour
         didx = now.day
@@ -159,7 +162,7 @@ class MotionSensor:
         while consumer_lock.locked():
             now = datetime.datetime.now()
             try:
-                item = sensor_motion_pop(resolution)
+                item = self.motion_pop(resolution)
                 print("POPED: {}".format(item))
                 if item[0] == MachineState.ON:
                     print("machine state changed to ON")
@@ -168,7 +171,7 @@ class MotionSensor:
                     print("machine state changed to OFF")
                     if laston != 0:
                         print("making calculation")
-                        data[hidx] += ((now.timestamp() - laston)/60)
+                        self.data[hidx] += ((now.timestamp() - laston)/60)
                     laston = 0
                     
             except queue.Empty:
@@ -178,22 +181,22 @@ class MotionSensor:
             if now.hour != hidx:
                 print("Hour changed")
                 if laston != 0:
-                    data[hidx] += ((now.timestamp() - laston)/60)
+                    self.data[hidx] += ((now.timestamp() - laston)/60)
                     laston = now.timestamp()
                 #switch index to next hour
                 hidx = now.hour
                 
             if now.day != didx:
                 print("Day changed")
-                sensor_motion_tofile(output,machname,data)
+                self.to_file(output,machname,data)
                 #reset data and start all over again
-                data = numpy.zeros(24,dtype = int)
+                self.data = numpy.zeros(24,dtype = int)
                 #switch index to next day and 0 hour
                 didx = now.day
                 hidx = now.hour #this probably be 0 
                 
         print("consumer thread quitting")
-        sensor_motion_tostdout(machname,data)
+        self.to_stdout(machname,data)
     
     """
     Functions that handles RBPI interrupt on PIN this functions 
@@ -207,7 +210,7 @@ class MotionSensor:
     def motion_push(state):
         ct = datetime.datetime.now()
         ts = ct.timestamp()
-        time_queue.put([state,ts])
+        self.time_queue.put([state,ts])
         return ts
 
     def motion_pop(resolution = 0.25):
@@ -246,3 +249,6 @@ class MotionSensor:
 #    sensor_motion_setup()
 #    
 #sensor_motion_exec()
+
+app = Application()
+app.run()
