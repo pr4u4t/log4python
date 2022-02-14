a84661820f7b34691a3270103e181380193ff120
Paweł Ciejka <pawel.ciejka@robco.pl>
2022-02-14 14:00:23 +0000

    added simple incomplete lock mechanism test

--- LockMechanism.py
+++ LockMechanism.py
@@ -23,3 +23,20 @@ class ThreadedStreamRequestHandler(socketserver.BaseRequestHandler):
 
 class ThreadedStreamServer(socketserver.ThreadingMixIn, socketserver.UnixStreamServer):
     pass
+
+if __name__ == "__main__":
+    print("Basic class test")
+    from ArgumentParser import parse_arguments
+    from ArgumentParser import check_environment
+    import sys
+    from time import sleep
+    
+    args = parse_arguments()
+    if not check_environment(args):
+        print("Failed to prepare running environment")
+        sys.exit(-1)
+    try:
+        print("TODO: test")
+    except:
+        print("print exception caught not good")
+        raise
